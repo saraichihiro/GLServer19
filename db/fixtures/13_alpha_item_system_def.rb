7c278227f58be6c324a01f07fda629758b3493b4
yoshikawa naoki <millennium_myonn@yahoo.co.jp>
2019-11-06 15:49:27 +0900

    [Add]seed値、追加
    _alpha_item_system_def.rbと _alpha_item_work_def.rb、
    _alpha_item_team_def.rbぞれぞれにシード値を追加

--- db/fixtures/13_alpha_item_system_def.rb
+++ db/fixtures/13_alpha_item_system_def.rb
@@ -1,2 +1,48 @@
 # -*- coding: utf-8 -*-
 
+#システム1/5 38
+AlphaItemDef.seed_once(:id,
+    { id: 38, dseq:'1', ddescription: 'システムを構成する主要コンポーネントの構造と関連(ソフトウェアアーキテクチャ)を設計する意義を理解している', alpha_state_def_id: 6 },
+    { id: 39, dseq:'2', ddescription: '品質(効率性、使用性、信頼性など)、開発効率、学習容易さで、アーキテクチャに求める要件を順位付けしている', alpha_state_def_id: 6 },
+    { id: 40, dseq:'3', ddescription: 'アーキテクチャを決定し、そのアーキテクチャが要件を満たしていることを確認している', alpha_state_def_id: 6 },
+    { id: 41, dseq:'4', ddescription: 'ソフトウェアフレームワーク(Rails)や、重要なライブラリ(jQuery)、開発用のプログラミング言語を決定している', alpha_state_def_id: 6 },
+    { id: 42, dseq:'5', ddescription: 'アーキテクチャ図を作成している', alpha_state_def_id: 6 },
+    { id: 43, dseq:'6', ddescription: 'オーナと開発チームはアーキテクチャがシンプルで、要件を満たしていることに同意している', alpha_state_def_id: 6 }
+)
+
+# システム2/5 44
+AlphaItemDef.seed_once(:id,
+    { id: 44, dseq:'1', ddescription: 'コーディング規約を作成し、チーム全員がそれに従うことに同意している', alpha_state_def_id: 7 },
+    { id: 45, dseq:'2', ddescription: 'プロダクトの主要機能の処理フローをアクティビティ図で記述している', alpha_state_def_id: 7 },
+    { id: 46, dseq:'3', ddescription: 'プロダクトの主要機能で使われるデータのスキーマ図を作成している', alpha_state_def_id: 7 },
+    { id: 47, dseq:'4', ddescription: 'プロダクトの主要機能の画面のレイアウトを作成している', alpha_state_def_id: 7 },
+    { id: 48, dseq:'5', ddescription: '運用環境(ハードウェア、ネットワーク環境、基本ソフトウェア、ミドルウェア、ライブラリなど)を、バージョンを含め決定している', alpha_state_def_id: 7 },
+    { id: 49, dseq:'6', ddescription: '運用環境をインストールでき、運用環境においてサンプルアプリをビルドし、実行できることを確認している', alpha_state_def_id: 7 },
+    { id: 50, dseq:'7', ddescription: 'オーナと開発チームは、概略設計案が要件を満たしていることを確認している', alpha_state_def_id: 7 }
+)
+
+# システム3/5 51
+AlphaItemDef.seed_once(:id,
+    { id: 51, dseq:'1', ddescription: 'メンバは単体テストを実施し、定義条件を満足したタスクをDoneとしている', alpha_state_def_id: 8 },
+    { id: 52, dseq:'2', ddescription: '開発チームは結合テストを実施し、定義条件を満足したタスクをDoneとしている', alpha_state_def_id: 8 },
+    { id: 53, dseq:'3', ddescription: 'オーナは、インクリメントを実行して機能を利用できる', alpha_state_def_id: 8 }
+)
+
+# システム4/5 54
+AlphaItemDef.seed_once(:id,
+    { id: 54, dseq:'1', ddescription: '運用環境において、共有されたソースを用いてインクリメントがビルドできている', alpha_state_def_id: 9 },
+    { id: 55, dseq:'2', ddescription: '運用環境において、機能テストが実施され、スプリントバックログのうちで、Doneした項目と未完了の項目を識別している', alpha_state_def_id: 9 },
+    { id: 56, dseq:'3', ddescription: 'ユーザストーリの実際の場面における様々な条件を考えて機能テストを実施している', alpha_state_def_id: 9 },
+    { id: 57, dseq:'4', ddescription: 'チーム以外の利用者は、運用環境においてインクリメントを実行して機能を利用できる', alpha_state_def_id: 9 }
+)
+
+# システム5/5 58
+AlphaItemDef.seed_once(:id,
+    { id: 58, dseq:'1', ddescription: 'システムの主要な機能がテストされている', alpha_state_def_id: 10 },
+    { id: 59, dseq:'2', ddescription: 'システムの処理能力や応答時間が要求通りであるかテストされている', alpha_state_def_id: 10 },
+    { id: 60, dseq:'3', ddescription: 'ユーザインタフェースが使いやすいかテストされている', alpha_state_def_id: 10 },
+    { id: 61, dseq:'4', ddescription: '高負荷状態（大量の入力データや同時多数ユーザなど）がテストされている', alpha_state_def_id: 10 },
+    { id: 62, dseq:'5', ddescription: 'エラー処理や回復機能が正常に動作するかテストされている', alpha_state_def_id: 10 },
+    { id: 63, dseq:'6', ddescription: 'ユーザドキュメントが利用できる', alpha_state_def_id: 10 },
+    { id: 64, dseq:'7', ddescription: 'リリース用のパッケージが作成され、その機能や品質レベルが文書化されている', alpha_state_def_id: 10 }
+)
--- db/fixtures/14_alpha_item_work_def.rb
+++ db/fixtures/14_alpha_item_work_def.rb
@@ -1,2 +1,51 @@
 # -*- coding: utf-8 -*-
 
+# 仕事1/5 65
+AlphaItemDef.seed_once(:id,
+    { id: 65, dseq:'1', ddescription: '全員がプロジェクトのビジョンを共有するためのイベントを開催している', alpha_state_def_id: 11 },
+    { id: 66, dseq:'2', ddescription: 'プロダクトバックログの優先順位の基準が定義されている', alpha_state_def_id: 11 },
+    { id: 67, dseq:'3', ddescription: 'プロジェクトのリスク(技術、人的リソース、機関など)が識別されている', alpha_state_def_id: 11 },
+    { id: 68, dseq:'4', ddescription: '開発環境と開発ツールを決定している', alpha_state_def_id: 11 }
+)
+
+# 仕事2/5 69
+AlphaItemDef.seed_once(:id,
+    { id: 69, dseq:'1', ddescription: '開発ツールを利用してアーキテクチャのサンプルアプリが作成でき、開発環境において実行できることを確認している', alpha_state_def_id: 12 },
+    { id: 70, dseq:'2', ddescription: '開発環境と開発ツールをインストールする方法が明確になっている', alpha_state_def_id: 12 },
+    { id: 71, dseq:'3', ddescription: 'チーム全員が、開発環境と開発ツールをインストールする方法を理解している', alpha_state_def_id: 12 },
+    { id: 72, dseq:'4', ddescription: 'チーム全員が開発環境と開発ツールを利用できる状態にある', alpha_state_def_id: 12 },
+    { id: 73, dseq:'5', ddescription: '用するアプリケーションフレームワークやライブラリの学習リソース（ウェブサイトや書籍など）のリストを作成している', alpha_state_def_id: 12 }
+)
+
+# 仕事3/5 74
+AlphaItemDef.seed_once(:id,
+    { id: 74, dseq:'1', ddescription: 'メンバは自分の意志でタスクを選択し、作業を進めている', alpha_state_def_id: 13 },
+    { id: 75, dseq:'2', ddescription: 'チームはタスクのDoneの条件を定義し、それを守ることに同意している', alpha_state_def_id: 13 },
+    { id: 76, dseq:'3', ddescription: 'メンバはタスクのDoneの定義に従ってタスクの進捗状況を更新している', alpha_state_def_id: 13 },
+    { id: 77, dseq:'4', ddescription: 'メンバはタスクの進捗を適切に更新しており、全員がそれを容易に閲覧できる', alpha_state_def_id: 13 },
+    { id: 78, dseq:'5', ddescription: 'ソースが共有されており、定期的に最新版に更新されている', alpha_state_def_id: 13 },
+    { id: 79, dseq:'6', ddescription: '共有されたソースを使ってシステムを一定期間ごとに運用環境でビルドしている', alpha_state_def_id: 13 },
+    { id: 80, dseq:'7', ddescription: 'バグ情報が共有されており、バグの対策状況が追跡できている', alpha_state_def_id: 13 }
+)
+
+# 仕事4/5 81
+AlphaItemDef.seed_once(:id,
+    { id: 81, dseq:'1', ddescription: 'タスクの終了時に、完了日、作業時間などを記録している', alpha_state_def_id: 14 },
+    { id: 82, dseq:'2', ddescription: 'チームのベロシティを計測している', alpha_state_def_id: 14 },
+    { id: 83, dseq:'3', ddescription: 'タスクの進捗を可視化するためにスプリントの残作業時間をグラフ化している', alpha_state_def_id: 14 },
+    { id: 84, dseq:'4', ddescription: 'タスクのDoneの定義が守られているかを検査している', alpha_state_def_id: 14 },
+    { id: 85, dseq:'5', ddescription: '共有されたソースが定期的に更新されているかを検査している', alpha_state_def_id: 14 },
+    { id: 86, dseq:'6', ddescription: '共有されたソースを使ってシステムを一定期間ごとにビルドできているかを検査している', alpha_state_def_id: 14 },
+    { id: 87, dseq:'7', ddescription: 'バグの対策状況が正しく追跡されていることを検査している', alpha_state_def_id: 14 }
+)
+
+# 仕事5/5 88
+AlphaItemDef.seed_once(:id,
+    { id: 88, dseq:'1', ddescription: 'ソフトウェア製品の外部品質の６つの特性を理解している', alpha_state_def_id: 15 },
+    { id: 89, dseq:'2', ddescription: 'ソフトウェアテストのプロセスと技法を理解している', alpha_state_def_id: 15 },
+    { id: 90, dseq:'3', ddescription: 'ソースがレビューされ、担当者以外の人が理解できている', alpha_state_def_id: 15 },
+    { id: 91, dseq:'4', ddescription: 'オーナはシステムの機能と品質がリリース可能なレベルであるかを合意している', alpha_state_def_id: 15 },
+    { id: 92, dseq:'5', ddescription: 'チームはリリースのために不足している機能や品質を識別している', alpha_state_def_id: 15 },
+    { id: 93, dseq:'6', ddescription: 'チームはリリースのために追加作業が必要かを検討している', alpha_state_def_id: 15 },
+    { id: 94, dseq:'7', ddescription: 'チームはリリースに必要な追加作業を実施している', alpha_state_def_id: 15 }
+)
